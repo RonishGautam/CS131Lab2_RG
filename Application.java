88f8ca18b079d6a69716169a01ca2cb122996f8c
RonishGautam <122306969+RonishGautam@users.noreply.github.com>
2023-02-23 11:14:28 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,25 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		        Wizard wizard = new Wizard(" Player ");
+		        
+		        wizard.setHealth(200);
+		        wizard.setKey(22);
+		        wizard.lock(11);
+		        wizard.unlock(11);
+		        wizard.takeDamage(199);
+		        
+		        
+		       System.out.println(wizard.toString() );
+		      
+		        
+		   		    }
+		
+ 
+		
+	}
+	
+
